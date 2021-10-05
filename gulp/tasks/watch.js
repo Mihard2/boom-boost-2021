daa678164af6bd0f87446df69b33a99f16809f22
Mihard2 <enot42@gmail.com>
2021-10-06 01:23:58 +0300

    added blocks upcoming & why choose

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ module.exports = () =>{
         $.gulp.watch('src/pug/**/*.pug', $.gulp.series('pug'));
         $.gulp.watch('src/static/scss/**/*.scss', $.gulp.series('sass'));
         $.gulp.watch('src/static/fonts/**/*', $.gulp.series('fonts'));
-        $.gulp.watch('src/static/img/**/*');
+        $.gulp.watch('src/static/img/**/*', $.gulp.series('image'));
         $.gulp.watch('src/static/js/**/*', $.gulp.series('scripts'));
         $.gulp.watch('src/static/localisation/**/*', $.gulp.series('localisation'));
     })
